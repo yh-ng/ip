39012a1b314748088f7db76be5dd152f8869a1b2
yh-ng <e0323678@u.nus.edu>
2020-09-01 18:24:00 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,7 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,9 +11,16 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
+        Scanner in = new Scanner(System.in);
+
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        System.out.println("");
+        String s = in.nextLine();
+        while(!s.equals("bye")) {
+            System.out.println(s);
+            s = in.nextLine();
+        }
         System.out.println("Bye. Hope to see you again soon!");
+
     }
 }

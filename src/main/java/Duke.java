57d437a051549af523ef71a99d48488f9315b200
yh-ng <e0323678@u.nus.edu>
2020-09-02 16:06:22 +0800

    Level-3. Added Task.java class.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,24 +16,51 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         String input = in.nextLine();
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int listCounter = 0;
         while(!input.equals("bye")) {
             if(input.equals("list")){
+                System.out.println("Here are the tasks in your list:");
                 for(int i = 0; i < listCounter; i++){
-                    System.out.println(Integer.toString(i+1) + ". " + list[i]);
+                    System.out.println(Integer.toString(i+1) + ". " + list[i].print());
                 }
-                input = in.nextLine();
-            }
-            else{
-                list[listCounter] = input;
-                System.out.println("added: " + input);
-                input = in.nextLine();
-                listCounter++;
             }
 
+
+            else if(validDoneInput(input,listCounter)){
+                int itemNumber = Integer.parseInt(input.substring(input.indexOf(' ') + 1));
+                list[itemNumber - 1].markDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(list[itemNumber - 1].print());
+            }
+            else{
+                list[listCounter] = new Task(input);
+                System.out.println("added: " + input);
+                listCounter++;
+            }
+            input = in.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
 
+    }
+
+    public static boolean validDoneInput(String s, int n) {
+        if(s.length() < 6){ // check if string is long enough
+            return false;
+        }
+        else if(!s.substring(0,4).equals("done")){ // check if string is "done ..."
+            return false;
+        }
+        int m = 0;
+        try {
+            m = Integer.parseInt(s.substring(5));
+        } catch(Exception e) {
+            return false;
+        }
+
+        if(m>n){
+            return false;
+        }
+        else return true;
     }
 }

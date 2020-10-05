1ca41ab9d7763ad275b549a93b8eb70b8927f300
yh-ng <e0323678@u.nus.edu>
2020-10-05 22:18:37 +0800

    Task.java

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,22 +1,42 @@
-public class Task {
+public class Task { //
     protected String description;
     protected boolean isDone;
+
 
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /*
+    Returns tick or X symbols depending on whether task is done or not
+     */
     public String getStatusIcon() {
-        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+        return (isDone ? "\u2713" : "\u2718");
     }
 
+    /*
+    Marks task as done
+     */
     public void markDone() {
         isDone = true;
     }
 
+
     public String toString() {
         return ("[" + this.getStatusIcon() + "] " + description);
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+
+    /*
+    Placeholder function. Formats the class based on the type, so can add back to list from saved file
+     */
+    public String formatInput() {
+        return "";
     }
 
 
